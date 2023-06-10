c6a1f44804ef6c0e2c55077252940ea0e91731c9
pakeg <pakegpakeg@gmail.com>
2023-06-10 11:57:36 +0300

    Update businessideas.com.ua-online-soft-languages-polish-#.js

--- businessideas.com.ua-online-soft-languages-polish-#.js
+++ businessideas.com.ua-online-soft-languages-polish-#.js
@@ -1,39 +1,39 @@
 // businessideas.com.ua/online-soft/languages/polish/#
-// block request for go.js 
-// add to console  
+// block request for go.js
+// add to console
 
 var $ur;
 var $ls;
 var $ln;
 var count = 0;
-function lessons($ur, $ls, $ln){
- $.ajax({
-          url: 'response1.php?ur='+$ur+'&start='+$ls+'&par='+$ln,
-          success: function(data) {
-            $('.result1').html(data);
-          }
-        });
-	$.ajax({
-          url: 'response2.php?ur='+$ur+'&start='+$ls+'&par='+$ln,
-          success: function(data) {
-            $('.result2').html(data);
-          }
-        });
+
+function lessons($ur, $ls, $ln) {
+  $.ajax({
+    url: "response1.php?ur=" + $ur + "&start=" + $ls + "&par=" + $ln,
+    success: function (data) {
+      $(".result1").html(data);
+    },
+  });
+  $.ajax({
+    url: "response2.php?ur=" + $ur + "&start=" + $ls + "&par=" + $ln,
+    success: function (data) {
+      $(".result2").html(data);
+    },
+  });
 }
 
- var $i=0;
- var $vd;
- var $va1;
- var $va2;
+var $i = 0;
+var $vd;
+var $va1;
+var $va2;
 
- $("a.but").live('click', function(){
-	$va2=this.id;
-	if($i==0){
-	$va1=this.id;
-	
-			}
-	});
-	$("div.but").live('click', function () {
+$("a.but").live("click", function () {
+  $va2 = this.id;
+  if ($i == 0) {
+    $va1 = this.id;
+  }
+});
+$("div.but").live("click", function () {
   if ($i == 0) {
     $vd = this.id;
     $(this).css({ background: "#7BE37B" });
@@ -46,8 +46,9 @@ function lessons($ur, $ls, $ln){
       if (count == $ln) {
         var str = "";
         var nls = $ls / 10 + 2;
-        var lesson = "sample" + $ur.slice(-1);
-        var nextLesson = "sample" + (+$ur.slice(-1) + 1);
+        var _ur = $ur.replace(/[a-zA-Z]/g, "").trim();
+        var lesson = "sample" + _ur;
+        var nextLesson = "sample" + (+_ur + 1);
 
         var main = $(".menubut").children("[class*=" + lesson + "]");
         var lenLesson = main.length;
@@ -70,15 +71,20 @@ function lessons($ur, $ls, $ln){
 });
 const menubut = document.querySelector(".menubut");
 
-
-menubut.onclick  = function (e) {
-    const target = e.target;
-    const arrTarget = target.getAttribute('class').substring(6).split('_'); 
-    $ur = "less"+arrTarget[0];
-    $ls = 10*(arrTarget[1]-1);
-    $ln = 10;
-    const title = document.querySelector(".sample"+arrTarget[0]+"_1").previousElementSibling.textContent.replace(/[0-9]|:|\./g, "").trim() +" урок "+ arrTarget[1];
-    const intro = document.querySelector(".intro").innerText = title;
-    target.style.backgroundColor = "#7BE37B";
-    lessons($ur, $ls, $ln);
-}
+menubut.onclick = function (e) {
+  const target = e.target;
+  const arrTarget = target.getAttribute("class").substring(6).split("_");
+  $ur = "less" + arrTarget[0];
+  $ls = 10 * (arrTarget[1] - 1);
+  $ln = 10;
+  const title =
+    document
+      .querySelector(".sample" + arrTarget[0] + "_1")
+      .previousElementSibling.textContent.replace(/[0-9]|:|\./g, "")
+      .trim() +
+    " урок " +
+    arrTarget[1];
+  const intro = (document.querySelector(".intro").innerText = title);
+  target.style.backgroundColor = "#7BE37B";
+  lessons($ur, $ls, $ln);
+};
